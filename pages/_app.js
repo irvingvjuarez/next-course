7567458ccc9a5d443ff0eaa43994b335bb090b60
Irving Juárez <irvingjuarez274@gmail.com>
2023-01-23 18:17:58 -0600

    Getting metrics from a next js app

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import Layout from "../containers/Layout"
 import "../style.css"
 
+export function reportWebVitals(metric) {
+	// console.log(metric)
+	// TODO: Doing something with these metrics
+}
+
 const App = ({ Component, pageProps }) => {
 	return (
 		<Layout>
